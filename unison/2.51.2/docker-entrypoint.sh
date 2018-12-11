01cc11dda4fbf7a4388c84959e76b4a018465583
jal <juan.alonso@advandoo.com>
2018-12-11 19:44:37 +0100

    [Fix] Setting "UNISON_USER" does not work if user already exists

--- unison/2.51.2/docker-entrypoint.sh
+++ unison/2.51.2/docker-entrypoint.sh
@@ -30,32 +30,30 @@ log_info "SYNC_MAX_INOTIFY_WATCHES: $SYNC_MAX_INOTIFY_WATCHES"
 log_info "SYNC_EXTRA_UNISON_PROFILE_OPTS: $SYNC_EXTRA_UNISON_PROFILE_OPTS"
 log_info "SYNC_NODELETE_SOURCE: $SYNC_NODELETE_SOURCE"
 
-if [ "$UNISON_USER" != "root" ]; then
-    HOME="/home/${UNISON_USER}"
-fi
-
 log_heading "Creating user ${UNISON_USER}."
-if [ ! $(getent group ${UNISON_GROUP}) ]; then
+if [[ ! $(getent group "${UNISON_GROUP}") ]]; then
     log_info "Group name ${UNISON_GROUP}"
     addgroup -g ${UNISON_GID} -S ${UNISON_GROUP}
 fi
-if [ ! $(getent passwd ${UNISON_USER}) ]; then
+if [[ ! $(getent passwd "${UNISON_USER}") ]]; then
     log_info "User name ${UNISON_USER}"
     adduser -u ${UNISON_UID} -D -S -G ${UNISON_GROUP} ${UNISON_USER}
-    mkdir -p ${HOME}/.unison
-    chown -R ${UNISON_USER}:${UNISON_GROUP} ${HOME}
 fi
+log_info "Setting up ~/.unison dir"
+HOME=$(eval echo ~${UNISON_USER})
+mkdir -p ${HOME}/.unison
+chown -R ${UNISON_USER}:${UNISON_GROUP} ${HOME}
 
 log_heading "Checking sync directories."
 log_info "SYNC_SOURCE_BASE_PATH: $SYNC_SOURCE_BASE_PATH"
 log_info "SYNC_DESTINATION_BASE_PATH: $SYNC_DESTINATION_BASE_PATH"
 
-[ -d "$SYNC_SOURCE_BASE_PATH" ] || log_error_exit "Source directory does not exist!"
-[ -d "$SYNC_DESTINATION_BASE_PATH" ] || log_error_exit "Destination directory does not exist!"
+[[ -d "$SYNC_SOURCE_BASE_PATH" ]] || log_error_exit "Source directory does not exist!"
+[[ -d "$SYNC_DESTINATION_BASE_PATH" ]] || log_error_exit "Destination directory does not exist!"
 [[ "$SYNC_SOURCE_BASE_PATH" != "$SYNC_DESTINATION_BASE_PATH" ]] || log_error_exit "Source and destination must be different directories!"
 
-if [ -n "${SYNC_MAX_INOTIFY_WATCHES}" ]; then
-  if [ -z "$(sysctl -p)" ]; then
+if [[ -n "${SYNC_MAX_INOTIFY_WATCHES}" ]]; then
+  if [[ -z "$(sysctl -p)" ]]; then
     echo fs.inotify.max_user_watches=$SYNC_MAX_INOTIFY_WATCHES | tee -a /etc/sysctl.conf && sysctl -p
   else
     log_info "Looks like /etc/sysctl.conf already has fs.inotify.max_user_watches defined."
@@ -64,7 +62,7 @@ if [ -n "${SYNC_MAX_INOTIFY_WATCHES}" ]; then
 fi
 
 prefer="prefer=newer"
-if [ -z "${SYNC_PREFER}" ]; then
+if [[ -z "${SYNC_PREFER}" ]]; then
   prefer="prefer=${SYNC_PREFER}"
 fi
 
